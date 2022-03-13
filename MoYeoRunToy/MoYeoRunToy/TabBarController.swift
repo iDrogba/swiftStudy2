5eadfc8dbe38db8b6df88157f3535daabee635a4
KimSangHyun <74828662+HimHeys@users.noreply.github.com>
2022-03-13 22:54:11 +0900

    Feat: tabBar shadow

---
+++ MoYeoRunToy/MoYeoRunToy/TabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarController.swift
+//  MoYeoRunToy
+//
+//  Created by 김상현 on 2022/03/13.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    func setUI() {
+        UITabBar.clearShadow()
+        tabBar.layer.applyShadow(color: .gray, alpha: 0.3, x: 0, y: 0, blur: 12)
+    }
+}
