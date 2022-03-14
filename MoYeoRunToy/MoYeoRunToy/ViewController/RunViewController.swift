3b59067bbcbbc5a30c809d3200e93eaf61724335
KimSangHyun <74828662+HimHeys@users.noreply.github.com>
2022-03-15 01:11:15 +0900

    Design: MainView 틀 완성

--- MoYeoRunToy/MoYeoRunToy/ViewController/RunViewController.swift
+++ MoYeoRunToy/MoYeoRunToy/ViewController/RunViewController.swift
@@ -24,3 +24,97 @@ class RunViewController: UIViewController{
     }
     
 }
+
+class RunViewController_NowPopular: UIViewController, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RunViewController_NowPopular_CollectionView_Cell", for: indexPath) as? RunViewController_NowPopular_CollectionView_Cell else {
+            return UICollectionViewCell()
+        }
+        cell.imageView.image = UIImage(named: "2.jpg")
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.bounds.width * 0.8
+        let height = collectionView.bounds.height * 0.9
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+class RunViewController_NowPopular_CollectionView_Cell : UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
+
+
+
+
+class RunViewController_NewMission: UIViewController,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RunViewController_NewMission_CollectionView_Cell", for: indexPath) as? RunViewController_NewMission_CollectionView_Cell else {
+            return UICollectionViewCell()
+        }
+        cell.imageView.image = UIImage(named: "2.jpg")
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.bounds.width * 0.4
+        let height = collectionView.bounds.height * 0.4
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+class RunViewController_NewMission_CollectionView_Cell: UICollectionViewCell{
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
+
+
+
+
+
+class RunViewController_LastRecord: UIViewController,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RunViewController_LastRecord_CollectionView_Cell", for: indexPath) as? RunViewController_LastRecord_CollectionView_Cell else {
+            return UICollectionViewCell()
+        }
+        cell.imageView.image = UIImage(named: "2.jpg")
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.bounds.width * 0.9
+        let height = collectionView.bounds.height * 0.4
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+class RunViewController_LastRecord_CollectionView_Cell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
