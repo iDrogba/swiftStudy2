b140eb18faba9c40643dfabbc28076db00460517
김상현 <kshy1019@naver.com>
2022-03-22 01:45:32 +0900

    Design: 홈 페이지 간격 조정 작업

--- MoYeoRunToy/MoYeoRunToy/ViewController/HomeViewController.swift
+++ MoYeoRunToy/MoYeoRunToy/ViewController/HomeViewController.swift
@@ -8,9 +8,18 @@
 import Foundation
 import UIKit
 
+// 고정 높이 320
 class HomeViewController: UIViewController{
     
+    let fixedHeight: CGFloat = 320
+    let screenHeight: CGFloat = UIScreen.main.bounds.height
+    var screenHeightForSetting: CGFloat = 0 // 적당한 이름이 생각이 안남..ㅠ
+    var multiplier: CGFloat = 0
+    
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var scrollViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var nowPopularCollectionView_HeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var newMissionCollectionView_HeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +27,23 @@ class HomeViewController: UIViewController{
     }
     
     func setUI(){
+        setProfileImageView()
+        setHeightConstraint()
+    }
+    
+    func setProfileImageView(){
         self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
         self.profileImageView.layer.masksToBounds = true
         self.profileImageView.layer.borderWidth = 0
     }
     
+    func setHeightConstraint(){
+        multiplier = (UIScreen.main.bounds.height > 800) ? 1 : 1.1
+        screenHeightForSetting = screenHeight * multiplier
+        
+        scrollViewHeightConstraint.constant = screenHeightForSetting + fixedHeight
+        nowPopularCollectionView_HeightConstraint.constant = screenHeightForSetting * 0.26
+        newMissionCollectionView_HeightConstraint.constant = screenHeightForSetting * 0.5 + 100
+        print(multiplier)
+    }
 }
