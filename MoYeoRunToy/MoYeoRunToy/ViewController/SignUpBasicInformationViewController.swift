202f2ad0c5f3d361b622ab18dc8f4207e48eda12
김상현 <kshy1019@naver.com>
2022-04-05 00:17:09 +0900

    회원가입 기본 정보 입력 화면 꾸미기 -1

---
+++ MoYeoRunToy/MoYeoRunToy/ViewController/SignUpBasicInformationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpBasicInformationViewController.swift
+//  MoYeoRunToy
+//
+//  Created by 김상현 on 2022/04/05.
+//
+
+import UIKit
+
+class SignUpBasicInformationViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var cameraSymbolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    func setUI(){
+        setProfileImageView()
+        setCameraSymbolButton()
+    }
+
+    func setProfileImageView(){
+        self.profileImageView.layer.cornerRadius = self.profileImageView.bounds.size.width * 0.5
+        self.profileImageView.layer.masksToBounds = true
+    }
+
+    func setCameraSymbolButton(){
+        self.cameraSymbolButton.layer.cornerRadius = self.cameraSymbolButton.frame.width / 2
+        self.cameraSymbolButton.layer.masksToBounds = true
+        self.cameraSymbolButton.layer.borderWidth = 0
+        cameraSymbolButton.imageView?.adjustsImageSizeForAccessibilityContentSizeCategory = true
+    }
+}
