bf68d6c3c4437621b0cef8187c5eb9af285015f7
김상현 <kshy1019@naver.com>
2022-04-16 21:06:13 +0900

    alert view 생성

---
+++ MoYeoRunToy/MoYeoRunToy/ViewController/SelectProfileAlertViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SelectProfileAlertViewController.swift
+//  MoYeoRunToy
+//
+//  Created by 김상현 on 2022/04/16.
+//
+
+import UIKit
+
+class SelectProfileImageAlertViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
