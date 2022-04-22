6e1c1f8cf39bbc4d9ea90bdebd1e5015c0e59d8b
KimSangHyun <74828662+iDrogba@users.noreply.github.com>
2022-04-22 23:38:58 +0900

    setGenderButtonUI logic -1

--- MoYeoRunToy/MoYeoRunToy/ViewController/SignUpBasicInformationViewController.swift
+++ MoYeoRunToy/MoYeoRunToy/ViewController/SignUpBasicInformationViewController.swift
@@ -14,14 +14,17 @@ class SignUpBasicInformationViewController: UIViewController {
     @IBOutlet weak var cameraSymbolButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var nickNameTextField: UITextField!
+    @IBOutlet weak var genderMaleButton: UIButton!
+    @IBOutlet weak var genderFemaleButton: UIButton!
+    var selectedGender: Int = 0
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
         setUI()
     }
     
-    func setUI() {
+    func setUI(){
         setProfileImageView()
         setCameraSymbolButton()
 
@@ -38,14 +41,58 @@ class SignUpBasicInformationViewController: UIViewController {
         }
     }
     
-    @IBAction func onTapCameraSymbolButton(_ sender: Any) {
+    @IBAction func onTapCameraSymbolButton(_ sender: Any){
         let storyboard = UIStoryboard(name: "SelectProfileImageAlertStoryboard", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "SelectProfileImageAlertViewController")
         viewController.modalPresentationStyle = .overCurrentContext
         self.present(viewController, animated: false)
     }
     
-    @IBAction func onTapMoveBackwardButton(_ sender: Any) {
+    @IBAction func onTapMoveBackwardButton(_ sender: Any){
         self.dismiss(animated: false)
+    }
+    
+    @IBAction func onTapGenderMaleButton(_ sender: Any) {
+        switch selectedGender {
+        case 0: selectedGender = 1
+        case 1: selectedGender = 0
+        case 2: selectedGender = 1
+        default: break
+        }
+        selectGender()
+    }
+    
+    @IBAction func onTapGenderFemaleButton(_ sender: Any) {
+        switch selectedGender {
+        case 0: selectedGender = 2
+        case 1: selectedGender = 2
+        case 2: selectedGender = 0
+        default: break
+        }
+        selectGender()
+    }
+    
+    func selectGender(){
+        switch selectedGender {
+        case 0: setButtonColorWhenNoSelectedGender()
+        case 1: setButtonColorWhenMale()
+        case 2: setButtonColorWhenFemale()
+        default: break
+        }
+        
+        func setButtonColorWhenNoSelectedGender() {
+            genderMaleButton.titleLabel?.textColor = UIColor.systemGray5
+            genderMaleButton.backgroundColor = UIColor.white
+            genderFemaleButton.titleLabel?.textColor = UIColor.systemGray5
+            genderFemaleButton.backgroundColor = UIColor.white
+        }
+        func setButtonColorWhenMale() {
+            genderMaleButton.titleLabel?.textColor = UIColor.white
+            genderMaleButton.backgroundColor = UIColor(named: "buttonColor1")
+        }
+        func setButtonColorWhenFemale() {
+            genderFemaleButton.titleLabel?.textColor = UIColor.white
+            genderFemaleButton.backgroundColor = UIColor(named: "buttonColor1")
+        }
     }
 }
